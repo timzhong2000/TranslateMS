173f48a10ca70a32399085f80c66fa36e5406def
Tim-Zhong-2000 <fengyu.fyxdfg@gmail.com>
2022-10-13 02:10:32 +0800

    feat: 添加 MapCache

---
+++ src/translator/cacheEngines/mapCache.ts
@@ -0,0 +1,40 @@
+import { Payload } from "../../types/Translator";
+import { CacheEngine } from "../abstract/cacheEngine";
+import { generatePayload } from "../../utils/generatePayload";
+
+export class MapCache extends CacheEngine {
+  db = new Map<string, Payload>();
+
+  constructor(private provider: string) {
+    super();
+  }
+
+  async fetch(
+    src: string,
+    srcLang: string,
+    destLang: string
+  ): Promise<Payload> {
+    const cache = this.db.get(
+      CacheEngine.generateHashKey(src, srcLang, destLang)
+    );
+    if (!cache) throw new Error("MISS");
+    return generatePayload(
+      true,
+      cache.level,
+      cache.src,
+      cache.dest,
+      cache.srcLang,
+      cache.destLang,
+      this.provider
+    );
+  }
+
+  insert(payload: Payload): void {
+    const hashKey = CacheEngine.generateHashKey(
+      payload.src,
+      payload.srcLang,
+      payload.destLang
+    );
+    this.db.set(hashKey, payload);
+  }
+}
